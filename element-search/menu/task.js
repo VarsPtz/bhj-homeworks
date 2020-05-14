b0c3afced997f1f8205c714b9ef815d287a77cb0
Alexandr Varygin <otz-job@mail.ru>
2020-05-14 19:22:42 +0300

    element-search menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,17 +2,21 @@ const mainMenuItems = document.querySelectorAll("ul.menu_main > li.menu__item > 
 
 Array.from(mainMenuItems).forEach(e => { 
  e.addEventListener("click", (event) => {
+  
   event.preventDefault();
+  
   let menuActive = document.querySelectorAll(
     "ul.menu.menu_main > li.menu__item > ul.menu.menu_sub.menu_active"
   );
+  
   if (e.closest(".menu__item").querySelector("ul.menu.menu_sub")) {
-   e.closest(".menu__item").querySelector("ul.menu.menu_sub").classList.toggle("menu_active");
-   if (menuActive.length != 0) {
-    menuActive[0].classList.remove("menu_active");
-   }
-   return false;
-  }  
- });
- 
+   e.closest(".menu__item").querySelector("ul.menu.menu_sub").classList.toggle("menu_active");  
+  } else {
+    location.href=e.href;
+  }
+
+  if (menuActive.length != 0) {
+    menuActive[0].classList.toggle("menu_active");
+  } 
+ }); 
 });
