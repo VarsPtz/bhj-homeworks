6463502dcdf41892324eebcc5bf6ce2a6a3ca87b
Alexandr Varygin <otz-job@mail.ru>
2020-05-19 18:17:23 +0300

    ads routers

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,33 +1,5 @@
 "use strict";
 
-// function advertizingRotator() {
- 
-//  const rotators = Array.from(document.querySelectorAll(".rotator__case_active")); 
- 
-//  rotators.forEach(rotator => {
-//   rotator.style.color = rotator.getAttribute("data-color");  
-//   rotator.classList.remove("rotator__case_active");
-//   if (rotator.nextElementSibling) {
-//    let dataColor = rotator.nextElementSibling.getAttribute("data-color");
-//    let dataSpeed = rotator.nextElementSibling.getAttribute("data-speed");
-//    setTimeout(() => {
-//     rotator.nextElementSibling.style.color = dataColor;
-//     rotator.nextElementSibling.classList.add("rotator__case_active");
-//    }, dataSpeed);   
-//   } else {
-//    let dataColor = rotator.parentElement.querySelector(".rotator__case").getAttribute("data-color");
-//    let dataSpeed = rotator.parentElement.querySelector(".rotator__case").getAttribute("data-speed");
-//    setTimeout(() => {
-//     rotator.parentElement.querySelector(".rotator__case").style.color = dataColor; 
-//     rotator.parentElement.querySelector(".rotator__case").classList.add("rotator__case_active");
-//    }, dataSpeed);   
-//   }
-//  });
-// }
-
-// setInterval(advertizingRotator, 1000);
-//ver.2
-
 const rotators = Array.from(document.getElementsByClassName("rotator"));
 
 rotators.forEach(rotator => {
@@ -53,7 +25,7 @@ rotators.forEach(rotator => {
     } else {
       rotatorCounter = 0;
     }
-    
+
     setTimeout(advertizingRotator, rotatorSpeed);
 
   }
