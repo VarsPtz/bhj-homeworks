c01d42c4b9566b592a2ab5fe5db2b02285ebb92e
Alexandr Varygin <otz-job@mail.ru>
2020-05-24 12:36:52 +0300

    todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,12 +3,36 @@
 const btnTaskAdd = document.getElementById("tasks__add");
 const taskInput = document.getElementById("task__input");
 const taskList = document.getElementById("tasks__list");
+let taskIdCounter;
+
+if (localStorage.getItem("lastTaskId")) {
+  for (let i = 0; i <= localStorage.getItem("lastTaskId"); i++) {
+    if (localStorage.getItem(`task${i}`)) {      
+      let data = localStorage.getItem(`task${i}`);
+      taskList.insertAdjacentHTML(
+        "beforeEnd",
+        `
+        <div class="task" data-task-id="task${i}">
+        <div class="task__title">
+        ${data}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+       </div>
+       `
+      );
+    }
+  }
+  taskIdCounter = +localStorage.getItem("lastTaskId") + 1;
+} else {
+  taskIdCounter = 0;
+}
 
 function addTask() {
  if (notEmpty(taskInput.value)) {
-  taskList.insertAdjacentHTML("beforeEnd", 
-   `
-   <div class="task">
+  taskList.insertAdjacentHTML(
+    "beforeEnd",
+    `
+   <div class="task" data-task-id="task${taskIdCounter}">
     <div class="task__title">
      ${taskInput.value}
     </div>
@@ -16,6 +40,8 @@ function addTask() {
    </div>
    `
   );
+  localStorage.setItem("lastTaskId", taskIdCounter);
+  localStorage.setItem(`task${taskIdCounter++}`, taskInput.value);
  } 
 }
 
@@ -39,6 +65,8 @@ taskInput.addEventListener("keydown", event => {
 
 taskList.addEventListener("click", event => {
  if (event.target.classList.contains("task__remove")) {
+  const taskId = event.target.closest(".task").getAttribute("data-task-id");
+  localStorage.removeItem(taskId);  
   event.target.closest(".task").remove();
  }
 });
